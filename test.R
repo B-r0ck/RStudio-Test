3b597bcdea1b135f81dbe774fc8af1d6b56e5f2b
B-r0ck <brittonaormond@gmail.com>
2023-04-20 16:16:30 -0400

    Clearing File Test

--- test.R
+++
@@ -1,9 +0,0 @@
-print("This file was created in RStudio")
-
-print("And now it lives on Github")
-
-print("this is a test sentence to track changes to the file")
-
-print("stage, commit, push")
-
-print("this is a test sentence to track changes to the file")
